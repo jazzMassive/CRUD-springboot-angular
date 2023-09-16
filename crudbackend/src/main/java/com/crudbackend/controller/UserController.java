f3afa633f0fb34aa6fcc38fbb771b153bf8cfa50
Jazz_Massive <emil.svaga@fer.hr>
2023-09-16 14:43:22 +0200

    Implemented Delete method in controller;

--- crudbackend/src/main/java/com/crudbackend/controller/UserController.java
+++ crudbackend/src/main/java/com/crudbackend/controller/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -53,5 +55,15 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
+    // delete user
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with id: " + id));
 
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 }
